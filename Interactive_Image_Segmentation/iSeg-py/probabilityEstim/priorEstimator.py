d7c4748b032027d0ba54202fa6a4499eafe0b401
Oskar <b00792974@essec.edu>
2023-03-15 17:48:48 +0100

    added doc

--- Interactive_Image_Segmentation/iSeg-py/probabilityEstim/priorEstimator.py
+++ Interactive_Image_Segmentation/iSeg-py/probabilityEstim/priorEstimator.py
@@ -3,19 +3,14 @@ import numpy as np
 class PriorEstimator:
 
     def __init__(self) -> None:
-        """ 
-        self.derivative_mat # 2, c, h, w
-        self.Theta # c, h, w
-        self.g
-            self.gamma # scalar
-            self.derivative_mat # 2, c, h, w
-         """
-        self.img = np.array([[[1, 4, 2], [5, 3, 2]], [[4, 1, 6], [2, 1, 1]]])
-
-        self.gradient = self._gradient_I(self.img)
-
-
+        pass
+    
     def _gradient_I(self, img):
+        """
+        Computes the gradient of an image. Dims: 2 x c x h x w
+
+        :param img: image of dimensions c x h x w
+        """
         diffs_x = np.diff(img, axis = 2)
         diffs_y = np.diff(img, axis = 1)
         
@@ -29,11 +24,24 @@ class PriorEstimator:
 
 
     def _g(self, img, gamma):
+        """
+        Computes the term g(x) (eq. 16).
+
+        :param img: image to segment of dimensions c x h x w
+        :param gamma: float
+        """
         grayscale_img = np.mean(img, axis=0)[None]
         abs_gradient_img = np.abs(self.gradient(grayscale_img))
         return np.exp(-abs_gradient_img*gamma)
     
     def _prior_energy(self, img, gamma, theta):
+        """
+        Computes the prior energy (eq. 21)
+
+        :param img: image to segment of dimensions c x h x w
+        :param gamma: float
+        :param theta: segmentation of the image of dimensions n x h x w
+        """
         d_Theta = self._gradient_I(theta)
         abs_d_Theta = np.abs(d_Theta)
         g = self._g(img, gamma)
